1509911d2fddab468aebb9222e9e7a51853a456f
Yomi Olatunji <mosesoluwayomi@gmail.com>
2023-04-07 13:07:21 +0100

    Add project files.

---
+++ SBSC.Wallet.BusinessCore/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SBSC.Wallet.BusinessCore
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddCoreService(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
